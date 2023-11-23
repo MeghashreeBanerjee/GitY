3ef4fcd47a6b88fb5d4fba80ac8e57b54a827c48
meghashree <meghashree.banerjee05@gmail.com>
2023-11-22 21:08:41 -0500

    Second Commit Small Test

---
+++ src/JavaStream1.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+import Testng.Test;
+
+public class JavaStream1 
+{
+
+	@Test
+	public static void main(String[] args) 
+	{
+		ArrayList<String> names = new ArrayList<String>();
+		names.add("Arizona");
+		names.add("Alabama");
+		names.add("Massachusettes");
+		names.add("California");
+		names.add("America");
+		int count=0;
+		
+		for(String nm : names)
+		{
+			System.out.println(nm);
+			
+			if(nm.startsWith("A"))
+				{
+					count++;
+				}
+		}
+		
+		System.out.println(count);
+	}
+	
+
+}
